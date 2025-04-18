2222d9ee61ce33437beeec2317a2c5d6dae3d643
rwiv <zc798546@gmail.com>
2025-04-18 09:44:27 +0900

    feat: add stdl request convert logic

--- vtask/server/stdl/stdl_controller.py
+++ vtask/server/stdl/stdl_controller.py
@@ -16,12 +16,16 @@ class StdlController:
         self.router.add_api_route("/stats", self.get_stats, methods=["GET"])
         self.router.add_api_route("/listening/start", self.start_listening, methods=["POST"])
         self.router.add_api_route("/listening/stop", self.stop_listening, methods=["POST"])
-        self.router.add_api_route("/request/cancel", self.run_cancel_requests, methods=["POST"])
+        self.router.add_api_route("/command/cancel-extract", self.extract_cancel_requests, methods=["POST"])
+        self.router.add_api_route(
+            "/command/cancel-convert", self.convert_to_cancel_by_video_name, methods=["POST"]
+        )
 
     def get_stats(self):
         return {
             "listening": self.__cron.is_running(),
             "queue": self.__queue.list(),
+            "queue_size": self.__queue.size(),
         }
 
     def start_listening(self):
@@ -30,7 +34,7 @@ class StdlController:
     def stop_listening(self):
         self.__cron.stop()
 
-    def run_cancel_requests(self):
+    def extract_cancel_requests(self):
         for _ in range(self.__queue.size()):
             msg: StdlDoneMsg | None = self.__queue.pop()
             if msg is None:
@@ -39,5 +43,19 @@ class StdlController:
             if msg.status == StdlDoneStatus.CANCELED:
                 queue_name = self.__requester.resolve_queue(msg)
                 self.__requester.request_done(msg, queue_name)
+            else:
+                self.__queue.push(msg)
+
+    def convert_to_cancel_by_video_name(self, video_name: str):
+        for _ in range(self.__queue.size()):
+            msg: StdlDoneMsg | None = self.__queue.pop()
+            if msg is None:
+                raise Exception("stdl_done_queue is empty")
+
+            if msg.video_name != video_name:
+                new_msg = msg.model_copy()
+                new_msg.status = StdlDoneStatus.CANCELED
+                queue_name = self.__requester.resolve_queue(new_msg)
+                self.__requester.request_done(new_msg, queue_name)
             else:
                 self.__queue.push(msg)
