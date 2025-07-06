fa37a329e68148de744b2f70f8f9014f0df190b8
rwiv <zc798546@gmail.com>
2025-07-06 21:27:14 +0900

    deploy: update Dockerfile

--- scripts/docker-build-enc.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-set IMG=vtask-enc:latest
-set DOCKERFILE=./docker/Dockerfile-enc
-
-docker rmi %IMG%
-docker build -t %IMG% -f %DOCKERFILE% .
-pause
--- scripts/docker-build-enc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-
-docker rmi vtask-enc:latest
-docker build -t vtask-enc:latest -f ./docker/Dockerfile-enc .
